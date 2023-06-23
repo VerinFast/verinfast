759f771b00125c64bf77e905a774eaeff96faba8
Sean T. Conrad <sean@startupos.dev>
2023-06-23 08:14:45 -0400

    read requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="verinfast",
     version="0.1",
     license="Proprietary",
     packages=find_packages(),
-    install_requires=[
-        'multimetric',
-        'PyYAML',
-        'requests',
-        'semgrep'
-    ],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'verinfast = verinfast.agent:main',
