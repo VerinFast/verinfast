ad2269a7493ec6d59ee0cfca600401f4bf6fdb3c
Jason Nichols <jason@startupos.dev>
2023-07-06 14:59:51 -0400

    gcp working ish

--- verinfast/cloud/gcp/blocks.py
+++ verinfast/cloud/gcp/blocks.py
@@ -1,17 +1,66 @@
-import os
-from google.cloud.monitoring_v3 import query, QueryServiceClient
+import json
+import time
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/jason/.config/gcloud/application_default_credentials.json"
+from google.cloud.monitoring_v3 import query, QueryServiceClient, MetricServiceClient, TimeInterval, ListTimeSeriesRequest
+from google.cloud import storage
 
 def getBlocks(sub_id:str):
-    client = QueryServiceClient()
-    q = query.Query(
-        client=client, 
-        project=sub_id, 
-        metric_type='storage.googleapis.com/storage/total_bytes',
-        days=1
-    )
-    df = q.as_dataframe()
-    print(df)
+    # Instantiates a client
+    storage_client = storage.Client(project=sub_id)
 
-getBlocks(sub_id="startupos-328814")
+    # List all the buckets available
+    client = MetricServiceClient()
+    # for bucket in storage_client.list_buckets():
+        # print(bucket)
+        # print(bucket.name)
+
+    now = time.time()
+    seconds = int(now)
+    nanos = int((now - seconds) * 10**9)
+    interval = TimeInterval(
+        {
+            "end_time": {"seconds": seconds, "nanos": nanos},
+            "start_time": {"seconds": (seconds - 300), "nanos": nanos},
+        }
+    )
+
+    results = client.list_time_series(
+        request={
+            "name": f"projects/{sub_id}",
+            "filter": f'metric.type = "storage.googleapis.com/storage/total_bytes"',
+            "interval": interval,
+            "view": ListTimeSeriesRequest.TimeSeriesView.FULL,
+        }
+    )
+    my_buckets = []
+    for result in results:
+        if result.resource and result.resource.labels:
+            bn = result.resource.labels["bucket_name"]
+            size = result.points[-1].value.double_value 
+            bucket = storage_client.get_bucket(bucket_or_name=bn)
+            rp = bucket.retention_period
+            my_bucket = {
+                "name": bn,
+                "size": size,
+                "retention": rp,
+                "public": False
+            }
+            configuration = bucket.iam_configuration
+            uniformEnabled = configuration['uniformBucketLevelAccess']['enabled']
+            iam = bucket.get_iam_policy()
+            permissions = []
+            blobs = bucket.list_blobs()
+            for bl in blobs:
+                i = bl.get_iam_policy()
+                for bin in i.bindings:
+                    print(bin)
+            for b in iam.bindings:
+                p = {"permission": b["role"], "roles": list(b["members"])}
+                permissions.append(p)
+                if "allUsers" in b["members"]:
+                    my_bucket["public"] = True
+            my_bucket["permissions"] = permissions
+            my_buckets.append(my_bucket)
+    return my_buckets
+    
+print(json.dumps(getBlocks(sub_id="startupos-328814"), indent=4))
