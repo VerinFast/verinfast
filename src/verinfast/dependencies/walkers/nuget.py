ab71da849a159d2636162fa1a219cd4677132bc3
Jason Nichols <jason@centricityinsights.com>
2023-08-11 16:18:10 -0400

    prep

--- src/verinfast/dependencies/walkers/nuget.py
+++ src/verinfast/dependencies/walkers/nuget.py
@@ -1,13 +1,15 @@
-from typing import TextIO
 import json
 import xml.etree.ElementTree as ET
 
 from verinfast.dependencies.walkers.classes import Walker, Entry
 
+
 class NuGetWalker(Walker):
-    def initialize(self, command: str=None):
+    def initialize(self, command: str = None):
         discoveryUrl = 'https://api.nuget.org/v3/index.json'
-        uselessList = json.loads(self.getUrl(discoveryUrl).content.decode('utf-8'))
+        uselessList = json.loads(
+            self.getUrl(discoveryUrl).content.decode('utf-8')
+        )
         for r in uselessList["resources"]:
             if "Catalog" in r["@type"]:
                 self.catalogUrl = r["@id"]
@@ -15,34 +17,43 @@ class NuGetWalker(Walker):
                 self.registrationUrl = r["@id"]
         if command:
             return super().initialize(command)
-    
-    def get_license(self, name:str, version:str)->str:
-        resp = json.loads(self.getUrl(f"{self.registrationUrl}{name}/{version}.json").content.decode('utf-8'))
+
+    def get_license(self, name: str, version: str) -> str:
+        resp = json.loads(
+            self.getUrl(f"{self.registrationUrl}{name}/{version}.json")
+                .content
+                .decode('utf-8')
+            )
         catalog_entry_url = resp["catalogEntry"]
-        catalog_entry = json.loads(self.getUrl(catalog_entry_url).content.decode("utf-8"))
+        catalog_entry = json.loads(
+            self.getUrl(catalog_entry_url)
+                .content
+                .decode("utf-8")
+        )
         if "licenseExpression" in catalog_entry:
             return catalog_entry["licenseExpression"]
         elif "licenseUrl" in catalog_entry:
             return catalog_entry["licenseUrl"]
         else:
             return ""
-        
-    def parse(self, file:str, expand=False):
-        # Reference: <PackageReference Include="Swashbuckle.AspNetCore" Version="6.5.0" />
+
+    def parse(self, file: str, expand=False):
+        # Reference: <PackageReference Include="Swashbuckle.AspNetCore" Version="6.5.0" /> # noqa: E501
         tree = ET.parse(file)
         x_path = "ItemGroup/PackageReference"
         dependencies = tree.findall(x_path)
         for d in dependencies:
             v = d["Version"]
             n = d["Include"]
-            l = self.get_license(name=n, version=v)
+            lic = self.get_license(name=n, version=v)
             e = Entry(
                 source="nuget",
                 name=n,
                 specifier="=="+v,
-                license=l,
+                license=lic,
             )
             self.entries.append(e)
+
 
 # Example: https://github.com/umbraco/Umbraco-CMS/pull/13787/files
 c_sharp_matches = ["*.csproj"]
