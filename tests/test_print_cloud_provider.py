aa0f17dc8050ea528b6a94e67ce5dcc08c40badc
Jason Nichols <jason@startupos.dev>
2024-01-08 13:48:50 +0000

    confirmed printable cloud

---
+++ tests/test_print_cloud_provider.py
@@ -0,0 +1,43 @@
+import json
+from unittest.mock import patch
+import os
+from pathlib import Path
+import shutil
+
+from verinfast.agent import Agent
+from verinfast.config import Config
+from verinfast.upload import Uploader
+from verinfast.utils.utils import DebugLog
+
+
+def print_cloud(config_file:str):
+    file_path = Path(__file__)
+    test_folder = file_path.parent.absolute()
+    agent = Agent()
+    config = Config()
+    config.cfg_path = str(test_folder.joinpath(config_file).absolute())
+    config.__init__()
+    # Test not overwritten
+    assert config.cfg_path == str(test_folder.joinpath(config_file).absolute())  # noqa: E501
+    assert config.modules.cloud
+    agent.config = config
+    for provider in agent.config.modules.cloud:
+        print(provider)
+
+@patch('verinfast.user.__get_input__', return_value='y')
+def test_gcp(self):
+    config_file="gcp_conf.yaml"
+    print_cloud(config_file=config_file)
+    assert True
+
+@patch('verinfast.user.__get_input__', return_value='y')
+def test_aws(self):
+    config_file="aws_conf.yaml"
+    print_cloud(config_file=config_file)
+    assert True
+
+@patch('verinfast.user.__get_input__', return_value='y')
+def test_aws_dash(self):
+    config_file="aws_dash.yaml"
+    print_cloud(config_file=config_file)
+    assert True
