4f1a040b673b1fecf48d03c10d56ab06c235ec77
Sean T. Conrad <seantconrad@gmail.com>
2024-02-03 13:13:45 -0500

    fixed unrelated unit test

--- tests/test_dry.py
+++ tests/test_dry.py
@@ -30,7 +30,7 @@ def test_no_config(self):
     agent.config.shouldUpload = True
     agent.debug = DebugLog(path=agent.config.output_dir, debug=False)
     agent.log = agent.debug.log
-    print(agent.debug.logFile)
+    agent.uploader.config = config.upload_config
     agent.scan()
     assert Path(results_dir).exists()
     files = os.listdir(results_dir)
