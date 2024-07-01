8f382fe50f9606eefd015f1b1dba42be03b448b8
Sean T. Conrad <seantconrad@gmail.com>
2024-07-01 10:40:50 -0400

    better file path

--- tests/test_repo_name.py
+++ tests/test_repo_name.py
@@ -1,8 +1,14 @@
 import json
+from pathlib import Path
 from verinfast.utils.utils import get_repo_name_url_and_branch
 
-
-with open("fixtures/url_support/test_repo_urls.json", "r") as f:
+file_path = Path(__file__)
+test_folder = file_path.parent.absolute()
+fixtures_dir = test_folder.joinpath("fixtures").absolute()
+url_list_path = str(
+    fixtures_dir.joinpath('url_support/test_repo_urls.json').absolute()
+)
+with open(url_list_path, "r") as f:
     url_list = json.load(f)
 
 
