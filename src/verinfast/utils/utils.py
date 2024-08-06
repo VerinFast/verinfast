f2726cd964c585cd980a59d57e6b449553dfbf4e
Sean T. Conrad <seantconrad@gmail.com>
2024-08-06 11:40:29 -0400

    Hopefully final

--- src/verinfast/utils/utils.py
+++ src/verinfast/utils/utils.py
@@ -50,14 +50,21 @@ def list_files(
 
 def std_exec(cmd: List[str], log=None):
     try:
-        return subprocess.check_output(cmd).decode('utf-8', errors='replace')
+        return subprocess.check_output(cmd).decode('utf-8')
     except (subprocess.CalledProcessError, UnicodeDecodeError) as e:
-        if log is not None:
-            log(tag="std_exec Error", msg=f"{e}, {cmd}")
-        else:
-            print(f"std_exec Error: {e}, {cmd}")
-        return ''
-
+        try:
+            return subprocess.check_output(
+                cmd, shell=True).decode(encoding='latin-1')
+        except Exception as e2:
+            if log is not None:
+                log(tag="std_exec replace Error", msg=f"{e2}, {cmd}")
+            else:
+                print(f"std_exec replace Error: {e2}, {cmd}")
+            if log is not None:
+                log(tag="std_exec Error", msg=f"{e}, {cmd}")
+            else:
+                print(f"std_exec Error: {e}, {cmd}")
+            return ''
 
 def escapeChars(text: str):
     fixedText = re.sub(r'([\"\{\}])', r'\\\1', text)
