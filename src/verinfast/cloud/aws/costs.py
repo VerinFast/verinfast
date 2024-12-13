35b3a359aa537afb5cacae934c2e0d13054f5361
ojokure <ojokuredim@gmail.com>
2024-12-13 20:24:16 +0100

    cloud scan pagination

--- src/verinfast/cloud/aws/costs.py
+++ src/verinfast/cloud/aws/costs.py
@@ -7,16 +7,22 @@ from verinfast.cloud.aws.get_profile import find_profile
 def runAws(targeted_account, start, end, path_to_output, log,
            profile=None, dry=False):
 
-    def _get_costs_and_usage(profile: str, aws_output_file: str):
-        cmd = f'''
+    def _get_costs_and_usage(profile: str,
+                             aws_output_file: str, next_token=None):
+        base_cmd = f'''
             aws ce get-cost-and-usage \
             --time-period Start={start},End={end} \
             --granularity=DAILY \
             --metrics "BlendedCost" \
             --group-by Type=DIMENSION,Key=SERVICE \
             --profile="{profile}" \
-            --output=json | cat
-        '''
+            --output=json'''
+
+        cmd = f'{base_cmd} --next-token="{next_token}" | cat'
+        if next_token:
+            cmd = f'{base_cmd} --next-token="{next_token}" | cat'
+        else:
+            cmd = f'{base_cmd} | cat'
 
         try:
             results = subprocess.run(
@@ -38,11 +44,13 @@ def runAws(targeted_account, start, end, path_to_output, log,
                 tag="AWS CLI")
             return None
 
-        obj = json.loads(text)
-        results_by_time = obj["ResultsByTime"]
+        return json.loads(text)
+
+    def _process_results(obj):
         charges = []
+        results_by_time = obj.get("ResultsByTime", [])
         for charge in results_by_time:
-            if charge["Groups"]:
+            if charge.get("Groups"):
                 for group in charge["Groups"]:
                     newCharge = {
                         "Date": charge["TimePeriod"]["Start"],
@@ -51,16 +59,44 @@ def runAws(targeted_account, start, end, path_to_output, log,
                         "Currency": group["Metrics"]["BlendedCost"]["Unit"]
                     }
                     charges.append(newCharge)
+        return charges
+
+    def _get_all_costs(profile: str, aws_output_file: str):
+        all_charges = []
+        next_token = None
+
+        while True:
+            # Get results for current page
+            obj = _get_costs_and_usage(profile, aws_output_file, next_token)
+            if not obj:
+                break
+
+            # Process and append results
+            charges = _process_results(obj)
+            all_charges.extend(charges)
+
+            # Check for next page
+            next_token = obj.get("NextPageToken")
+            if not next_token:
+                break
+
+            log(msg=f"Fetching next page of results with token: "
+                f"{next_token[:10]}...",
+                tag="AWS CLI")
+
+        # Create final upload object
         upload = {
             "metadata": {
                 "provider": "aws",
                 "account": str(targeted_account)
             },
-            "data": charges
+            "data": all_charges
         }
 
+        # Write results to file
         with open(aws_output_file, 'w') as outfile:
             outfile.write(json.dumps(upload, indent=4))
+
         return aws_output_file
 
     if profile is None:
@@ -75,7 +111,8 @@ def runAws(targeted_account, start, end, path_to_output, log,
         path_to_output,
         f'aws-cost-{targeted_account}.json'
     )
+
     if not dry:
-        _get_costs_and_usage(profile, output_file)
+        _get_all_costs(profile, output_file)
 
     return output_file
