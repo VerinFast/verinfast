983f4db1c9846487d3548635ce39ed606c4e5cf7
Sean T. Conrad <seantconrad@gmail.com>
2025-01-13 11:21:13 -0500

    took out separate dir

--- tests/test_dry.py
+++ tests/test_dry.py
@@ -9,8 +9,7 @@ from verinfast.utils.utils import DebugLog
 
 file_path = Path(__file__)
 test_folder = file_path.parent.absolute()
-# To run unit tests in parallel, this test needs its own results directory
-results_dir = test_folder.joinpath("results_dry").absolute()
+results_dir = test_folder.joinpath("results").absolute()
 str_path = str(test_folder.joinpath('str_conf.yaml').absolute())
 
 
