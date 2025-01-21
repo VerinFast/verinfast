95ca00557f69798f049de814ae59965caba01eb9
ojokure <ojokuredim@gmail.com>
2025-01-21 14:18:00 +0100

    sys info using hyfetch

--- src/verinfast/upload.py
+++ src/verinfast/upload.py
@@ -39,7 +39,8 @@ class Uploader:
             "scan_id": f"{report}{code_sep}",
             "logs": f"{report}/agent_logs",
             "err_stats": f"{report}/agent_err/stats_err",
-            "err_findings": f"{report}/agent_err/findings_err"
+            "err_findings": f"{report}/agent_err/findings_err",
+            "system_info": f"{report}/system_info"
         }
 
         if report is None:
