36f891a349823614b748b6cd9200716ddb0f7e1b
Jason Nichols <github@ayl.us>
2025-03-05 17:45:20 -0500

    Update agent.py
    move license reporting and report for verinfast, not just modernmetric

--- src/verinfast/agent.py
+++ src/verinfast/agent.py
@@ -94,6 +94,7 @@ class Agent:
                 f.write(output)
 
     def scan(self):
+        report_license(self.config.reportId, self.config, "verinfast")
         if self.config.modules is not None:
             if self.config.modules.code is not None:
 
@@ -426,7 +427,6 @@ class Agent:
                 template_definition["filelist"] = filelist
                 custom_args = [f"--file={stats_input_file}", f"--output={stats_output_file}"]
                 modernmetric(custom_args=custom_args, license_identifier=self.config.reportId)
-                report_license(self.config.reportId, self.config, "modernmetric")
 
                 with open(stats_output_file, 'r') as f:
                     template_definition["stats"] = json.load(f)
