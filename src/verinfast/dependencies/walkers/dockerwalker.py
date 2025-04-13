b91164ce8e3dd646d5cf2cf2b86e8c9ac1efd9e6
Jason Nichols <github@ayl.us>
2025-04-13 00:53:00 -0400

    Update dockerwalker.py

--- src/verinfast/dependencies/walkers/dockerwalker.py
+++ src/verinfast/dependencies/walkers/dockerwalker.py
@@ -31,7 +31,7 @@ class DockerWalker(Walker):
         elif ":" in name:
             [name, specifier] = name.split(":")
 
-        e = Entry(name=name or address, specifier=specifier, source=source, required_by=file)
+        e = Entry(name=name or address+"-", specifier=specifier, source=source, required_by=file)
         return e
 
     def parse(self, file: str, expand=False):
