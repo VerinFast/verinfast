6605088abb04dfda40da2cdccbbbe61f99d24037
Sean T. Conrad <seantconrad@gmail.com>
2025-05-15 09:03:01 -0400

    Updated unit tests

--- tests/test_local.py
+++ tests/test_local.py
@@ -9,25 +9,29 @@ from verinfast.agent import Agent
 def test_local_scan(self):
     file_path = Path(__file__)
     test_folder = file_path.parent
-    output_path = test_folder.joinpath("results").joinpath("tsx_test.stats.json")
+    stats_path = test_folder.joinpath("results").joinpath("tsx_test.stats.json")
+    size_path = test_folder.joinpath("results").joinpath("tsx_test.sizes.json")
+    findings_path = test_folder.joinpath("results").joinpath("tsx_test.findings.json")
+    dependencies_path = test_folder.joinpath("results").joinpath("tsx_test.dependencies.json")
     tsx_path = test_folder.joinpath("tsx_test")
 
     agent = Agent()
-    agent.config.runGit = False
     agent.config.dry = False
-    agent.config.runPygount = False
-    agent.config.runScan = False
-    agent.config.runSizes = False
-    agent.config.runStats = True
+    agent.config.shouldUpload = False
+    agent.config.runGit = True
     agent.config.output_dir = test_folder.joinpath("results")
     os.makedirs(agent.config.output_dir, exist_ok=True)
     agent.config.config["local_repos"] = [tsx_path.absolute().as_posix()]
 
     agent.scanRepos()
 
-    with open(output_path) as output_file:
+    with open(stats_path) as output_file:
         output = json.load(output_file)
         assert output is not None
+
+    assert os.path.exists(size_path)
+    assert os.path.exists(findings_path)
+    assert os.path.exists(dependencies_path)
 
     return None
 
@@ -36,24 +40,28 @@ def test_local_scan(self):
 def test_local_scan_branch(self):
     file_path = Path(__file__)
     test_folder = file_path.parent
-    output_path = test_folder.joinpath("results").joinpath("tsx_test.stats.json")
+    stats_path = test_folder.joinpath("results").joinpath("tsx_test.stats.json")
+    size_path = test_folder.joinpath("results").joinpath("tsx_test.sizes.json")
+    findings_path = test_folder.joinpath("results").joinpath("tsx_test.findings.json")
+    dependencies_path = test_folder.joinpath("results").joinpath("tsx_test.dependencies.json")
     tsx_path = test_folder.joinpath("tsx_test")
 
     agent = Agent()
-    agent.config.runGit = False
     agent.config.dry = False
-    agent.config.runPygount = False
-    agent.config.runScan = False
-    agent.config.runSizes = False
-    agent.config.runStats = True
+    agent.config.shouldUpload = False
+    agent.config.runGit = True
     agent.config.output_dir = test_folder.joinpath("results")
     os.makedirs(agent.config.output_dir, exist_ok=True)
     agent.config.config["local_repos"] = [tsx_path.absolute().as_posix() + "@main"]
 
     agent.scanRepos()
 
-    with open(output_path) as output_file:
+    with open(stats_path) as output_file:
         output = json.load(output_file)
         assert output is not None
+
+    assert os.path.exists(size_path)
+    assert os.path.exists(findings_path)
+    assert os.path.exists(dependencies_path)
 
     return None
